73eadc0ad3a7d1d5a2069fd6b3421d994e1ac877
NevoStrauss <nevostrauss@gmail.com>
2021-01-04 14:04:19 +0200

    first

---
+++ cmake-build-debug/CMakeFiles/BGRSclient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BGRSclient"
+  "BGRSclient.pdb"
+  "CMakeFiles/BGRSclient.dir/src/BGRSclient.cpp.o"
+  "CMakeFiles/BGRSclient.dir/src/connectionHandler.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BGRSclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
